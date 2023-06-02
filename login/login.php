f2e2f60bc10676bd4a03daaa00065821b4ac58cf
Rikelmen Viegas <rikelminviegas12@gmail.com>
2023-06-02 07:18:42 -0300

    //*Mudei a estrutura do codigo, e tb a cor de fundo

--- login/login.php
+++ login/login.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="login.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 
 <body>
     <div class="login-container">
-        <img src="../Img/SYMBOL.png" alt="">
+        <img src="../Img/logo.png"alt="">
         <h1>Luffy Vet Pet</h1>
         <form action="../scripts/validar_login.php" method="post">
             <label for="Usuario">Usuario:</label>
