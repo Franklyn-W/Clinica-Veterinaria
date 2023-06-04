e9e667a44ee48d822b0b6a89836ff5327cb486b0
Rikelmen Viegas <rikelminviegas12@gmail.com>
2023-06-04 13:29:17 -0300

    RIkelmen
    alteração na barra de menu

--- menu_funcionario/menu/menu.js
+++ menu_funcionario/menu/menu.js
@@ -1,14 +1,10 @@
-function toggleMenu() {
-    var menuContainer = document.querySelector('.menu-container');
-    menuContainer.classList.toggle('open');
-  }
+document.addEventListener("DOMContentLoaded", function () {
+    const submenuToggle = document.querySelector("#novo-cadastro");
+    const submenu = document.querySelector(".submenu ul");
   
-  var menuToggle = document.querySelector('.menu-toggle');
-  menuToggle.addEventListener('click', toggleMenu);
-  
-  var novoCadastroLink = document.querySelector('#novo-cadastro');
-  var novoCadastroSubmenu = document.querySelector('.submenu ul');
-  novoCadastroLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    novoCadastroSubmenu.classList.toggle('hidden');
+    submenuToggle.addEventListener("click", function (event) {
+      event.preventDefault();
+      submenu.classList.toggle("hidden");
+    });
   });
+  
